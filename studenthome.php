1a3b444c07c902cc0eb82ac02236232b0466e0f8
sharath kumar <sharathkumardaroor@gmail.com>
2018-10-14 08:16:11 +0530

    Add files via upload

--- studenthome.php
+++ studenthome.php
@@ -1,10 +1,18 @@
-<?php
-session_start();
-include 'studentlogin.php';
-?>
-<html>
-    <head>
-        <link rel="stylesheet" href="external.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Training and Placement Cell</title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="css/mdb.min.css" rel="stylesheet">
+  <!-- Your custom styles (optional) -->
+  <link href="css/style.css" rel="stylesheet">
         <script>
             history.pushState(null, null, location.href);
             window.onpopstate = function () 
@@ -15,18 +23,30 @@ include 'studentlogin.php';
         
     </head>
     <body>
-        <?php include 'studentlogin.php';
-        ?>
-            <table>
-                <p style="font-size: 18px; color: #28ff00;"><br><b>OBJECTIVE:</b><br>
-The major objective of campus placement is to identify the talented and qualified professionals before they complete their 
-education.It provide employment opportunities to the students who are perusing or in the final stage of completing the 
-course. This process reduces the time for an industry to pick the candidates according to their need.It is a cumbersome 
-activity and hence majority of the companies find it difficult to trace the right talent. Many students do not understand the importance of placement training that is 
-being imparted, whether it is an aptitude training or soft skills. They show the least interest in this due to various factors viz., 
-projects, assignments or more of activities loaded by the colleges as part of their curriculum thinking that it is not useful. It 
-is the responsibility of the companies training on placement to make the students equipped on all aspects of career development along 
-with creating a very good impact in them which makes them feel every minute they spend in the placement training session is worth 
-being there and will help them in getting placed in their dream companies.<br></p>
+        <?php session_start();
+        include 'studentlogin.php';?>
+        <div class="jumbotron" style="height: 100vh;">
+            <h5 class="animated fadeIn mb-3">OBJECTIVE:</h5>
+            <br>
+            <p>
+                The major objective of campus placement is to identify the talented and qualified professionals before they complete their 
+                education.It provide employment opportunities to the students who are perusing or in the final stage of completing the 
+                course. This process reduces the time for an industry to pick the candidates according to their need.It is a cumbersome 
+                activity and hence majority of the companies find it difficult to trace the right talent. Many students do not understand the importance of placement training that is 
+                being imparted, whether it is an aptitude training or soft skills. They show the least interest in this due to various factors viz., 
+                projects, assignments or more of activities loaded by the colleges as part of their curriculum thinking that it is not useful. It 
+                is the responsibility of the companies training on placement to make the students equipped on all aspects of career development along 
+                with creating a very good impact in them which makes them feel every minute they spend in the placement training session is worth 
+                being there and will help them in getting placed in their dream companies.<br></p>
+            </p>
+        </div>
+        <!-- JQuery -->
+  <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="js/mdb.min.js"></script>
     </body>
 </html>
